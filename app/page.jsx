b4e1fc4a3543337402d2b1328eb230b7867e367a
Shane Phillips <shane@loongphillips.com>
2023-05-25 16:02:51 -0400

    homepage now client-side, data fetching moved

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,33 @@
+"use client";
+import { useState, useEffect } from "react";
+import LoadingPage from "./loading";
 import Link from "next/link";
 import Courses from "./components/courses";
 
 const HomePage = () => {
+	//client-side components are more interactive, require regular React stuff like useEffect and useState
+	const [courses, setCourses] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchCourses = async () => {
+			const res = await fetch("/api/courses"); //gets the courses data
+			const data = await res.json();
+			setCourses(data); //puts the data in a State variable
+			setLoading(false);
+		};
+		fetchCourses();
+	}, []);
+
+	if (loading) {
+		return <LoadingPage />;
+	}
 	return (
-		<div>
+		<>
 			<h1>Welcome to Hell</h1>
-			<Courses />
-		</div>
+			<Courses courses={courses} />
+			{/* data is no longer fetched in Courses.jsx, it's fetched here and passed into Courses.jsx as a prop, both her and on the Courses.jsx page */}
+		</>
 	);
 };
 
